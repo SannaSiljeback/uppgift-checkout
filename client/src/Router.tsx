f72f90038a357df287c57b774c930f3c3c5947ff
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-04 13:51:37 +0200

    router is wokring

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,8 +1,4 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Login } from "./components/Login";
-import { Logout } from "./components/Logout";
-import { Payment } from "./components/Payment";
-import { Register } from "./components/Register";
 import { Confirmation } from "./pages/Confirmation";
 import { Home } from "./pages/Home";
 import { Layout } from "./pages/Layout";
@@ -16,26 +12,6 @@ export const Router = createBrowserRouter([{
         {
             path: "/",
             element: <Home />,
-            errorElement: <NotFound />,
-        },
-        {
-            path: "/register",
-            element: <Register />,
-            errorElement: <NotFound />,
-        },
-        {
-            path: "/login",
-            element: <Login />,
-            errorElement: <NotFound />,
-        },
-        {
-            path: "/logout",
-            element: <Logout />,
-            errorElement: <NotFound />,
-        },
-        {
-            path: "/payment",
-            element: <Payment />,
             errorElement: <NotFound />,
         },
         {
