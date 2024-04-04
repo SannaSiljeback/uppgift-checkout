5109a77f3992606bc25ddeadc7aef66154e70639
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-04 18:46:59 +0200

    starting product router

--- server/resources/products/products.controllers.js
+++ server/resources/products/products.controllers.js
@@ -1,5 +1,18 @@
-// const getProducts = async (req, res) => {
-//     //get anrop?
-// };
+const getProducts = async (req, res) => {
+    //get anrop?
+    const stripe = initStripe();
 
-// module.exports = { getProducts };
+    try {
+        const products = await stripe.products.list({
+            limit: 3,
+          });
+
+        res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json("Can not fetch products");
+    }
+ 
+};
+
+module.exports = { getProducts };
--- server/resources/products/products.router.js
+++ server/resources/products/products.router.js
@@ -1,7 +1,7 @@
-// const express = require("express");
-// const { getProducts } = require("./products.controllers");
-// const router = express.Router();
+const express = require("express");
+const { getProducts } = require("./products.controllers");
+const router = express.Router();
 
-// router.get("/products", getProducts);
+router.get("/products", getProducts);
 
-// exports.router = router;
+exports.router = router;
