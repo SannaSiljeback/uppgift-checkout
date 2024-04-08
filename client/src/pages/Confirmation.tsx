da36353c07a632b2acf94f2e46e53807a257304f
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-08 11:18:00 +0200

    orders.json is working

--- client/src/pages/Confirmation.tsx
+++ client/src/pages/Confirmation.tsx
@@ -1,9 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const Confirmation = () => {
 
     const [verified, setVerified] = useState(false);
     const [isLoading, setIsLoading] = useState(true); // inte perfekt att loading står hela tiden, men är så just nu
+
+    useEffect(() => {
+        const verifySession = async () => {
+            console.log("verifying session");
+
+            let sessionId;
+
+            const dataFromLs = localStorage.getItem("sessionId");
+            if (dataFromLs) {
+                sessionId = JSON.parse(dataFromLs);
+            }
+
+            const response = await fetch("http://localhost:3001/payments/verify-session", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ sessionId }),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                setVerified(data.verified);
+                setIsLoading(false);
+            }
+            
+            
+        }
+        verifySession();
+
+    }); // tom array??
 
     return (
         <>
--- server/stripe/stripe.controllers.js
+++ server/stripe/stripe.controllers.js
@@ -1,4 +1,5 @@
 const initStripe = require("../stripe");
+const fs = require("fs").promises;
 
 const createCheckoutSession = async (req, res) => {
 
@@ -8,7 +9,7 @@ const createCheckoutSession = async (req, res) => {
 
   const session = await stripe.checkout.sessions.create({
     mode: "payment",
-    customer: "cus_PruslmAylNykoy", //stripe ID här som ej är hårdkodat
+    customer: "cus_PruslmAylNykoy", //stripe ID här som ej är hårdkodat i framtiden
     line_items: cart.map(item => {
       return {
         price: item.product,
@@ -16,7 +17,7 @@ const createCheckoutSession = async (req, res) => {
       }
     }), //kundvagnen som kommer från clienten ska matcha det vi skickar ut från stripe och förmodlingen behöver vi göra en map emellan
     success_url: "http://localhost:5173/confirmation",
-    cancel_url: "http://localhost:5173", //ska denna också gå någonstans??
+    cancel_url: "http://localhost:5173",
   });
 
     res.status(200).json({url: session.url, sessionId: session.id});
@@ -29,6 +30,33 @@ const verifySession = async (req, res) => {
  
   
   const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+  if (session.payment_status === "paid") {
+    const lineItems = await stripe.checkout.sessions.listLineItems(sessionId);
+
+    const order = {
+      orderNumber: Math.floor(Math.random() * 1000000),
+      customerName: session.customer_details.email,
+      products: lineItems.data,
+      total: session.amount_total,
+      date: new Date(),
+    }
+    
+    try {
+      let orders = [];
+      try {
+        orders = JSON.parse(await fs.readFile("./data/orders.json"));
+      } catch (error) {
+        console.log("No orders found");
+      }
+      orders.push(order);
+      await fs.writeFile("./data/orders.json", JSON.stringify(orders, null, 2));
+    } catch (error) {
+      console.log(error);
+    }
+    res.status(200).json({ verified: true});
+
+  }
 
   console.log(session);
 };
