5dd295d5178f1445d1cfc571e0610f3d9839d5b8
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-04-11 18:51:15 +0200

    more design

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -8,34 +8,36 @@ export const Cart = () => {
 
   return (
     <>
-    <div className="bigCartContainer">
-      <h2>Your coffee cart</h2>
-      <ul>
-        
-        {cart.map((product) => (
-          
-          <div key={product.product.id} className="smallCartContainer">
-            <h3>{product.product.name}</h3>
-            <div className="itemContainer">
-            <img
-              src={product.product.images[0]}
-              alt={product.product.name}
-              style={{ width: "80px" }}
-            />
-            <p className="quantity">
-              {product.quantity} st -{" "}
-              {product.product.default_price.unit_amount / 100} SEK
-            </p>
-            <div onClick={() => removeFromCart(product.product)} className="delete">
-              <AiOutlineDelete />
+      <div className="bigCartContainer">
+        <h2>Your coffee cart</h2>
+        <ul>
+          {cart.map((product) => (
+            <div key={product.product.id} className="smallCartContainer">
+              <h3>{product.product.name}</h3>
+              <div className="itemContainer">
+                <img
+                  src={product.product.images[0]}
+                  alt={product.product.name}
+                  style={{ width: "80px" }}
+                />
+                <div className="quantityContainer">
+                  <p className="quantity">
+                    {product.quantity} st -{" "}
+                    {product.product.default_price.unit_amount / 100} SEK
+                  </p>
+                  <div
+                    onClick={() => removeFromCart(product.product)}
+                    className="delete"
+                  >
+                    <AiOutlineDelete />
+                  </div>
+                </div>
+              </div>
             </div>
-            </div>
-          </div>
-        ))}
-      
-      </ul>
+          ))}
+        </ul>
 
-      <Payment />
+        <Payment />
       </div>
     </>
   );
